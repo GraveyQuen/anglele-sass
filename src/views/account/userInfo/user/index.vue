8783bf9d9b1730347b24a399d6c92420bc38a637
well kim <505144350@qq.com>
2018-10-19 18:11:01 +0800

    修改密码，校验两次密码是否一致

--- src/views/account/userInfo/user/index.vue
+++ src/views/account/userInfo/user/index.vue
@@ -21,78 +21,83 @@
 </template>
 
 <script>
-  import {
-    mapGetters,
-    mapActions
-  } from 'vuex';
-  
-  import * as types from '@/store/types'
-  export default {
-    data() {
-      return {
-        dataApi: {
-          newPassword: '',
-          newPasswordAgain: ''
-        },
-        rule: {
-          newPassword: [{
+import { mapGetters, mapActions } from "vuex";
+
+import * as types from "@/store/types";
+export default {
+  data() {
+    return {
+      dataApi: {
+        newPassword: "",
+        newPasswordAgain: ""
+      },
+      rule: {
+        newPassword: [
+          {
             required: true,
-            message: '新密码不能为空',
-            trigger: 'blur'
-          }],
-          newPasswordAgain: [{
+            message: "新密码不能为空",
+            trigger: "blur"
+          }
+        ],
+        newPasswordAgain: [
+          {
             required: true,
-            message: '确认新密码不能为空',
-            trigger: 'blur'
-          }]
-        }
+            message: "确认新密码不能为空",
+            trigger: "blur"
+          }
+        ]
       }
-    },
-    computed: {
-      userInfo() {
-        return this.$store.state.base.loginInfo;
-      }
-    },
-    methods: {
-      ...mapActions(['loginOut']),
-      reset(name) {
-        let _this = this;
-        this.$refs[name].validate((valid) => {
-          if (valid) {
+    };
+  },
+  computed: {
+    userInfo() {
+      return this.$store.state.base.loginInfo;
+    }
+  },
+  methods: {
+    ...mapActions(["loginOut"]),
+    reset(name) {
+      let _this = this;
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          if (this.dataApi.newPassword === this.dataApi.newPasswordAgain) {
             this.loading = true;
             let params = this.$clearData(this.dataApi);
-            params.newPassword = this.$md5(params.newPassword)
-            params.newPasswordAgain = this.$md5(params.newPasswordAgain)
+            params.newPassword = this.$md5(params.newPassword);
+            params.newPasswordAgain = this.$md5(params.newPasswordAgain);
             this.$http.post(this.$api.changePass, params).then(res => {
               if (res.code === 1000) {
                 this.$Modal.success({
-                  title: '修改密码',
-                  content: '密码修改成功，请重新登录',
+                  title: "修改密码",
+                  content: "密码修改成功，请重新登录",
                   onOk() {
                     _this.$store.commit(types.LOGOUT);
-                    window.location.href = '/login';
+                    window.location.href = "/login";
                   }
                 });
               } else {
                 this.$Message.error(res.message);
               }
               this.loading = false;
-            })
+            });
           } else {
-            this.$Message.error('表单验证失败');
+            this.$Message.error("两次密码输入不一致");
           }
-        })
-      }
+        } else {
+          this.$Message.error("表单验证失败");
+        }
+      });
     }
   }
+};
 </script>
 
 <style lang='less' scoped>
-  .page-box {
-    border: 1px solid #dae2e8;
-    border-radius: 5px;
-    background: #fff;
-    position: relative;
-    padding: 20px;
-  }
+.page-box {
+  border: 1px solid #dae2e8;
+  border-radius: 5px;
+  background: #fff;
+  position: relative;
+  padding: 20px;
+}
 </style>
