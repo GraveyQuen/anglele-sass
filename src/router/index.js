e63312845f93b8afa1cb0fca17784b20a6a291a8
well kim <505144350@qq.com>
2018-11-02 14:43:06 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import {
   Modal
 } from 'iview'
 import routes from './routers'
+import { format } from 'url';
 
 Vue.use(Router);
 
@@ -40,7 +41,10 @@ router.beforeEach((to, from, next) => {
     if (!token && to.name !== LOGIN_PAGE_NAME) {
       // 未登录且要跳转的页面不是登录页
       next({
-        name: LOGIN_PAGE_NAME // 跳转到登录页
+        name: LOGIN_PAGE_NAME, // 跳转到登录页,
+        query: {
+          redirect: to.fullPath
+        }
       })
     } else if (!token && to.name === LOGIN_PAGE_NAME) {
       // 未登陆且要跳转的页面是登录页
